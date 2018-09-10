3c5470830597d1614e52ea147cfe6844b6305c47
3116004698-欧思良 <1329084482@qq.com>
2018-09-10 21:52:31 +0800

    提交

--- src/pers/WC.java
+++ src/pers/WC.java
@@ -1,5 +1,9 @@
 package pers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Scanner;
 
 public class WC {
@@ -8,36 +12,59 @@ public class WC {
 
 	public WC() {
 		// while (true) {
-		System.out.println("\t WC.exe 使用说明:\n");
-		System.out.println("-c 文件      （返回该文件的字符数）\n");
-		System.out.println("-w 文件      （返回该文件的词的数目）\n");
-		System.out.println("-l 文件      （返回该文件的行数）\n");
-		System.out.println("-a 文件      （返回该文件的代码行 / 空行 / 注释行）\n");
-		System.out.println("-s 文件      （递归处理）\n"); // ***************************
-		getIn = new Scanner(System.in);
-		if (getIn.hasNext()) {
-			command = getIn.nextLine();
-		}
-		String[] key = command.split(" |-");
-		String function = key[0];
-		String fileName = key[1];
-		switch (function) {
-		case "c":
-			charsCount(fileName);
-		case "w":
-			wordsCount(fileName);
-		case "l":
-			rowsCount(fileName);
-		case "a":
-			complexCount(fileName);
-		default:
-			System.out.println("输入有误请重新输入！\n");
-		}
+		// System.out.println("\t WC.exe 使用说明:\n");
+		// System.out.println("-c 文件 （返回该文件的字符数）\n");
+		// System.out.println("-w 文件 （返回该文件的词的数目）\n");
+		// System.out.println("-l 文件 （返回该文件的行数）\n");
+		// System.out.println("-a 文件 （返回该文件的代码行 / 空行 / 注释行）\n");
+		// System.out.println("-s 文件 （递归处理）\n"); // ***************************
+		// getIn = new Scanner(System.in);
+		// if (getIn.hasNext()) {
+		// command = getIn.nextLine();
+		// }
+		// String[] key = command.split(" ");
+		// String function = key[0];
+		// String fileName = key[1];
+		// switch (function) {
+		// case "c":
+		// charsCount(fileName);
+		// case "w":
+		// wordsCount(fileName);
+		// case "l":
+		// rowsCount(fileName);
+		// case "a":
+		// complexCount(fileName);
+		// default:
+		// System.out.println("输入有误请重新输入！\n");
+		// }
+		// }
+		Scanner b = new Scanner(System.in);
+		String a = String.valueOf(b);
+		charsCount(a); // 测试用
+		wordsCount(a); // 测试用
+		rowsCount(a); // 测试用
+		complexCount(a); // 测试用
 	}
-	// }
-
+          
+             
 	int charsCount(String fileName) { // 返回该文件的字符数
-		return 555;
+
+		int charsNum = 0;
+		File file = new File(fileName);
+		Reader readFile = null;
+		try {
+			int tempchar;
+			readFile = new InputStreamReader(new FileInputStream(file));
+			while ((tempchar = readFile.read()) != -1) {
+				if ((char) tempchar != '\r' && (char) tempchar != '\n') {
+					charsNum++;
+				}
+			}
+			readFile.close();
+		} catch (Exception e) {
+			System.out.println("输入的文件不存在");
+		}
+		return charsNum;
 	}
 
 	int wordsCount(String fileName) { // 返回该文件的词的数目
@@ -51,6 +78,4 @@ public class WC {
 	int complexCount(String fileName) { // 返回该文件的代码行 / 空行 / 注释行
 		return 0;
 	}
-	// int charCount(){
-	// return 0;}
 }
