2e7d7db82e44237d80ebef0eb88f7cc8dea59ecc
3116004698-欧思良 <1329084482@qq.com>
2018-09-10 23:15:01 +0800

    提交记录

--- src/pers/Main.java
+++ src/pers/Main.java
@@ -1,7 +1,38 @@
 package pers;
 
+import java.util.Scanner;
+
 public class Main {
-           public static void main(String args[]){
-        	   new WC( );
-           }
+	public static void main(String args[]) {
+		String command = null;
+		String function = null;
+		String path = null;
+		WC wc = new WC();
+		System.out.println("\t WC.exe 使用说明:\n");
+		System.out.println("-c 文件 （返回该文件的字符数）\n");
+		System.out.println("-w 文件 （返回该文件的词的数目）\n");
+		System.out.println("-l 文件 （返回该文件的行数）\n");
+		System.out.println("-a 文件 （返回该文件的代码行 / 空行 / 注释行）\n");
+		System.out.println("-s 文件 （递归处理）\n"); // ***************************
+		Scanner getIn = new Scanner(System.in);
+		if (getIn.hasNext()) {
+			command = getIn.next(); // 获取指令
+		}
+		String[] commands = command.split(" "); // 拆分用户输入的指令
+		function = commands[0]; // command前半部分为功能选择
+		path = commands[1]; //// command后半部分为文件路径 ///出现了数组下标越界的问题
+
+		if (function.equals("-c"))
+			wc.charsCount(path);
+		else if (function.equals("-w"))
+			wc.wordsCount(path);
+		else if (function.equals("-l"))
+			wc.rowsCount(path);
+		else if (function.equals("-a"))
+			wc.complexCount(path);
+		// else if(){}
+		else
+			System.out.println("用户输入不合法，请重新输入！");
+
+	}
 }
--- src/pers/WC.java
+++ src/pers/WC.java
@@ -7,46 +7,7 @@ import java.io.Reader;
 import java.util.Scanner;
 
 public class WC {
-	Scanner getIn = null;
-	String command = null;
-
-	public WC() {
-		// while (true) {
-		// System.out.println("\t WC.exe 使用说明:\n");
-		// System.out.println("-c 文件 （返回该文件的字符数）\n");
-		// System.out.println("-w 文件 （返回该文件的词的数目）\n");
-		// System.out.println("-l 文件 （返回该文件的行数）\n");
-		// System.out.println("-a 文件 （返回该文件的代码行 / 空行 / 注释行）\n");
-		// System.out.println("-s 文件 （递归处理）\n"); // ***************************
-		// getIn = new Scanner(System.in);
-		// if (getIn.hasNext()) {
-		// command = getIn.nextLine();
-		// }
-		// String[] key = command.split(" ");
-		// String function = key[0];
-		// String fileName = key[1];
-		// switch (function) {
-		// case "c":
-		// charsCount(fileName);
-		// case "w":
-		// wordsCount(fileName);
-		// case "l":
-		// rowsCount(fileName);
-		// case "a":
-		// complexCount(fileName);
-		// default:
-		// System.out.println("输入有误请重新输入！\n");
-		// }
-		// }
-		Scanner b = new Scanner(System.in);
-		String a = String.valueOf(b);
-		charsCount(a); // 测试用
-		wordsCount(a); // 测试用
-		rowsCount(a); // 测试用
-		complexCount(a); // 测试用
-	}
-          
-             
+                    
 	int charsCount(String fileName) { // 返回该文件的字符数
 
 		int charsNum = 0;
