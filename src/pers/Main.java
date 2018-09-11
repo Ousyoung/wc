d0401be13bb27ce509c8a152ef0963d9ab8dbe37
3116004698-欧思良 <1329084482@qq.com>
2018-09-11 21:01:32 +0800

    提交记录

--- src/pers/Main.java
+++ src/pers/Main.java
@@ -1,9 +1,10 @@
 package pers;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		String command = null;
 		String function = null;
 		String path = null;
@@ -14,11 +15,15 @@ public class Main {
 		System.out.println("-l 文件 （返回该文件的行数）\n");
 		System.out.println("-a 文件 （返回该文件的代码行 / 空行 / 注释行）\n");
 		System.out.println("-s 文件 （递归处理）\n"); // ***************************
+		System.out.print("请输入合理的查询命令:\n");
+		while(true){
+		
 		Scanner getIn = new Scanner(System.in);
 		if (getIn.hasNext()) {
-			command = getIn.next(); // 获取指令
+			command = getIn.nextLine(); // 获取指令
 		}
-		String[] commands = command.split(" "); // 拆分用户输入的指令
+		String[] commands = new String[2];
+		commands = command.split(" ", 2); // 拆分用户输入的指令
 		function = commands[0]; // command前半部分为功能选择
 		path = commands[1]; //// command后半部分为文件路径 ///出现了数组下标越界的问题
 
@@ -36,3 +41,4 @@ public class Main {
 
 	}
 }
+	}
--- src/pers/WC.java
+++ src/pers/WC.java
@@ -1,14 +1,18 @@
 package pers;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class WC {
-                    
-	int charsCount(String fileName) { // 返回该文件的字符数
+
+	void charsCount(String fileName) { // 返回该文件的字符数
 
 		int charsNum = 0;
 		File file = new File(fileName);
@@ -23,20 +27,72 @@ public class WC {
 			}
 			readFile.close();
 		} catch (Exception e) {
-			System.out.println("输入的文件不存在");
+			System.out.println("***系统提示：找不到指定的文件！请重新输入：");
 		}
-		return charsNum;
+		System.out.println("文件路径：" + fileName + "  文件的字符数为： " + charsNum);
 	}
 
-	int wordsCount(String fileName) { // 返回该文件的词的数目
-		return 0;
+	void wordsCount(String fileName) { // 返回该文件的词的数目
+		System.out.println("************************************");
 	}
 
-	int rowsCount(String fileName) { // 返回该文件的行数
-		return 0;
+	void rowsCount(String fileName) { // 返回该文件的行数
+		File file = new File(fileName);
+		Reader readFile = null;
+		int lineNum = 1;
+		try {
+			readFile = new InputStreamReader(new FileInputStream(file));
+			int tempchar;
+			while ((tempchar = readFile.read()) != -1) {
+				if ((char) tempchar == '\n') {
+					lineNum++;
+				}
+			}
+			readFile.close();
+		} catch (Exception e) {
+			System.out.println("***系统提示：找不到指定的文件！请重新输入：");
+		}
+		System.out.println("文件路径：" + fileName + "  文件的行数为： " + lineNum);
 	}
 
-	int complexCount(String fileName) { // 返回该文件的代码行 / 空行 / 注释行
-		return 0;
+	void complexCount(String fileName) throws IOException { // 返回该文件的代码行 / 空行 /
+															// 注释行
+		File file = new File(fileName);
+		BufferedReader bufr = null;
+		String line = null;
+		int codeLine = 0;
+		int blankLine = 0;
+		int noteLine = 0;    
+		
+		Pattern codePattern = Pattern.compile("(?!import|package).+;\\s*(((//)|(/\\*+)).*)*",
+				Pattern.MULTILINE + Pattern.DOTALL); // 代码行匹配器（以分号结束为一行有效语句,但不包括import和package语句）
+                                             /////匹配代码行时出现了错误
+	
+
+		Pattern blankPattern = Pattern.compile("^\\s*$"); // 空白行匹配器（匹配回车、tab键、空格）
+		
+		Pattern notePattern = Pattern.compile("((//)|(/\\*+)|((^\\s)*\\*)|((^\\s)*\\*+/))+",
+				Pattern.MULTILINE + Pattern.DOTALL); // 注释匹配器(匹配单行、多行、文档注释)
+
+		
+		try {
+			bufr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			while ((line = bufr.readLine()) != null) {
+				if (codePattern.matcher(line).matches()) {
+					codeLine++;
+				}
+				if (blankPattern.matcher(line).find()) {
+					blankLine++;
+				}
+				if (notePattern.matcher(line).find()) {
+					noteLine++;
+				}			
+			}
+			bufr.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("***系统提示：找不到指定的文件！请重新输入：");
+		}
+		System.out.println("文件路径：" + fileName + "  文件的代码行 / 空行 / 注释行数为： " +codeLine+"/"+blankLine+"/"+noteLine);
 	}
 }
